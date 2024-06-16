c344f44f0fff17081f0f63751c7ff38b831b5449
kurtCoding <kjulien7790@gmail.com>
2024-06-16 11:34:14 -0400

    added switch case with funtionality and added name and description to package.json

--- index.js
+++ index.js
@@ -5,7 +5,38 @@ const { create, index, show, update, destroy } = require("./src/snacks");
 function run() {
     let snacks = readJSONFile("./data", "snacks.json");
     let updatedSnack = [];
+    let writeToFile = false;
+    
+    const action = process.argv[2];
+    const snack = process.argv[3];
 
+    switch (action) {
+        case "create":
+            updatedSnack = create(snacks, snack);
+            writeToFile = true;
+            break;
+        case "index":
+            const allSnacks = index(snacks)
+            console.log(allSnacks);
+            break;
+        case "show":
+            const oneSnack = show(snacks, snack);
+            console.log(oneSnack);
+            break;
+        case "update":
+            updatedSnack = update(snacks, snack, process.argv[4]);
+            writeToFile = true;
+            break;
+        case "destroy":
+            updatedSnack = destroy(snacks, snack);
+            writeToFile = true;
+            break;
+        default:
+            console.log("There was an error");
+    }
+    if (writeToFile) {
+        writeJSONFile("./data", "snacks.json", updatedSnack);
+    }
 }
 
 run();
